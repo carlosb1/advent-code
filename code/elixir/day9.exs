4224d3073aa0c79bccce7310b7864a455bc54d67
carlosb1 <mcflurry0@gmail.com>
2023-08-19 20:37:54 +0200

    Added solution for day9
    Signed-off-by: carlosb1 <mcflurry0@gmail.com>

--- code/elixir/day9.exs
+++ code/elixir/day9.exs
@@ -1,10 +1,19 @@
 defmodule Position do
   defstruct y: 0, x: 0
+
+  def distance(position, compare_position) do
+    :math.sqrt(
+      :math.pow(position.x - compare_position.x, 2) +
+        :math.pow(position.y - compare_position.y, 2)
+    )
+  end
 end
 
 defmodule PositionState do
   defstruct current_position: %Position{},
-            list_positions: [%Position{}]
+            tail_position: %Position{},
+            list_tail_position: [],
+            list_positions: []
 end
 
 defmodule Day9 do
@@ -19,58 +28,80 @@ defmodule Day9 do
         {direction, step}
       end)
       |> Enum.reduce(%PositionState{}, fn input, acc ->
-        IO.inspect(input, label: :input)
-        # IO.inspect(acc, label: :acc)
+        {_, steps} = input
 
-        current_position = acc.current_position
-
-        # Checking head position
-        {new_current_position, tail_steps} =
+        step_position =
           case input do
-            {"U", steps} ->
-              tail_steps =
-                1..(steps - 1)
-                |> Enum.map(fn step -> %{current_position | y: current_position.y + step} end)
-
-              {%{current_position | y: current_position.y + steps}, tail_steps}
-
-            {"D", steps} ->
-              tail_steps =
-                1..(steps - 1)
-                |> Enum.map(fn step -> %{current_position | y: current_position.y - step} end)
-
-              {%{current_position | y: current_position.y - steps}, tail_steps}
-
-            {"L", steps} ->
-              tail_steps =
-                1..(steps - 1)
-                |> Enum.map(fn step -> %{current_position | x: current_position.x - step} end)
-
-              {%{current_position | x: current_position.x - steps}, tail_steps}
-
-            {"R", steps} ->
-              tail_steps =
-                1..(steps - 1)
-                |> Enum.map(fn step -> %{current_position | x: current_position.x + step} end)
-
-              {%{current_position | x: current_position.x + steps}, tail_steps}
+            {"U", _} -> %Position{y: 1}
+            {"D", _} -> %Position{y: -1}
+            {"L", _} -> %Position{x: -1}
+            {"R", _} -> %Position{x: 1}
           end
 
-        # IO.inspect(tail_steps, label: :tail_step)
+        acc =
+          1..steps
+          |> Enum.reduce(acc, fn reduce_one_movement, acc_state_one_movement ->
+            current_position = acc_state_one_movement.current_position
 
-        acc = %{
-          acc
-          | current_position: new_current_position,
-            list_positions: acc.list_positions ++ tail_steps
-        }
+            new_current_position = %{
+              current_position
+              | y: current_position.y + step_position.y,
+                x: current_position.x + step_position.x
+            }
+
+            distance =
+              acc_state_one_movement.tail_position |> Position.distance(new_current_position)
+
+            {new_tail_position, added_list_position, added_tail_positions} =
+              if distance >= 2.0 do
+                # here another loop for all thew new queue to review all the tails
+                # the removed value must be the current option
+                # siz = acc_state_one_movement.list_tail_position |> Enum.count()
+                ##  added_tail_positions =
+                # if siz == 9 do
+                #  added_position = acc_state_one_movement.list_tail_position |> Enum.at(8)
+
+                #  new_tail_positions =
+                #    acc_state_one_movement.list_tail_position
+                #    |> List.delete_at(8)
+                #    |> List.insert_at(0, current_position)
+
+                #  {current_position, [added_position], new_tail_positions}
+                # else
+                #  new_tail_positions =
+                #    acc_state_one_movement.list_tail_position
+                #    |> List.insert_at(0, current_position)
+
+                #  {current_position, [], new_tail_positions}
+                # end
+
+                ## TODO uncomment this for part 1 and not apply if result
+                {current_position, [current_position], [current_position]}
+              else
+                {acc_state_one_movement.tail_position, [],
+                 acc_state_one_movement.list_tail_position}
+              end
+
+            # IO.inspect(distance, label: :distance)
+
+            # add condition
+
+            acc_state_one_movement = %{
+              acc_state_one_movement
+              | list_positions: acc_state_one_movement.list_positions ++ added_list_position,
+                tail_position: new_tail_position,
+                list_tail_position: added_tail_positions,
+                current_position: new_current_position
+            }
+
+            acc_state_one_movement
+          end)
 
         acc
       end)
-      |> IO.inspect()
 
     result.list_positions
     |> Enum.uniq()
-    |> IO.inspect()
     |> Enum.count()
     |> IO.inspect()
   end
